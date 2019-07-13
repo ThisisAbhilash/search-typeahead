8a3bd71305fdcecec2ec665f937c52e76daf0f4e
Abhilash Thakur <abhilash.thakur@furlenco.com>
2019-07-13 22:34:21 +0530

    Implemented Debounce

--- js/search.js
+++ js/search.js
@@ -1,80 +1,86 @@
 
-document.addEventListener('DOMContentLoaded', () => {
-  fetchData();
-});
+const DELAY = 500; // 0.5 sec debounce
+document.addEventListener('DOMContentLoaded', () => fetchData());
 
 async function fetchData() {
-  try {
-    const loaderElement = document.getElementById('loader'),
-      searchBody = document.getElementById('search');
+  const loaderElement = document.getElementById('loader'),
+    searchBody = document.getElementById('search');
 
+  try {
     const data = await (await fetch('/api/fetch-data')).json();
     typeAhead(document.getElementById("search-input"), data);
-    loaderElement.style.display = 'none';
-    searchBody.style.display = 'block';
+
   } catch (error) {
-    loaderElement.style.display = 'none';
-    searchBody.style.display = 'block';
     console.log(`[Error]:- failed to load data ${JSON.stringify(error)}`);
   }
+  loaderElement.style.display = 'none';
+  searchBody.style.display = 'block';
 }
 
 function isMatch(item, text) {
   var result = false;
   for(var key in item) {
-    if(typeof item[key] === 'object') {
-      result = result || isMatch(item[key], text);
-    }
-    else {
-      result = result || item[key].toLowerCase().indexOf(text.toLowerCase()) > -1;
-    }
+    if(typeof item[key] === 'object') result = result || isMatch(item[key], text);
+    else result = result || item[key].toLowerCase().indexOf(text.toLowerCase()) > -1;
   }
   return result;
 }
 
+function debounce(func, delay) { 
+  let debounceTimer;
+  return function() { 
+    const context = this
+    const args = arguments 
+    clearTimeout(debounceTimer);
+    debounceTimer = setTimeout(() => func.apply(context, args), delay); 
+  } 
+}
+
 function typeAhead(inputElement, allAvailableOptions) {
   var currentFocus;
-  inputElement.addEventListener("input", function() {
-      var divElement, contentElement, index, searchedContent = this.value, noMatchFound = true;
-      closeAllLists();
-      if (!searchedContent) { return false;}
-      currentFocus = -1;
+  inputElement.addEventListener("input", debounce(inputChangeHandler, DELAY));
 
-      divElement = document.createElement("div");
-      divElement.setAttribute("id", this.id + "typeahead-list");
-      divElement.setAttribute("class", "typeahead-items");
-      divElement.classList.add('apply-hover');
+  function inputChangeHandler() {
+    var divElement, contentElement, index, searchedContent = this.value, noMatchFound = true;
+    closeAllLists();
+    if (!searchedContent) { return false;}
+    currentFocus = -1;
 
-      this.parentNode.appendChild(divElement);
-      
-      for (index = 0; index < allAvailableOptions.length; index++) {
-        if(isMatch(allAvailableOptions[index], searchedContent)) {
+    divElement = document.createElement("div");
+    divElement.setAttribute("id", this.id + "typeahead-list");
+    divElement.setAttribute("class", "typeahead-items");
+    divElement.classList.add('apply-hover');
 
-          contentElement = document.createElement("div");
-          contentElement.setAttribute("class", "card");
+    this.parentNode.appendChild(divElement);
+    
+    for (index = 0; index < allAvailableOptions.length; index++) {
+      if(isMatch(allAvailableOptions[index], searchedContent)) {
 
-          contentElement = createCard(contentElement, allAvailableOptions[index], searchedContent);
-
-          contentElement.addEventListener("click", function(allAvailableOptions, index) {
-            return function() {
-              alert('Selected Id:- ' + allAvailableOptions[index].id);
-              closeAllLists();
-              inputElement.value = '';
-            }
-          }(allAvailableOptions, index));
-
-          divElement.appendChild(contentElement);
-          noMatchFound = false;
-        }
-      }
-      if(noMatchFound) {
         contentElement = document.createElement("div");
         contentElement.setAttribute("class", "card");
-        contentElement.innerHTML = "<span>No Search Results.</span>";
-        divElement.style.pointerEvents = 'none';
+
+        contentElement = createCard(contentElement, allAvailableOptions[index], searchedContent);
+
+        contentElement.addEventListener("click", function(allAvailableOptions, index) {
+          return function() {
+            alert('Selected Id:- ' + allAvailableOptions[index].id);
+            closeAllLists();
+            inputElement.value = '';
+          }
+        }(allAvailableOptions, index));
+
         divElement.appendChild(contentElement);
+        noMatchFound = false;
       }
-  });
+    }
+    if(noMatchFound) {
+      contentElement = document.createElement("div");
+      contentElement.setAttribute("class", "card");
+      contentElement.innerHTML = "<span>No Search Results.</span>";
+      divElement.style.pointerEvents = 'none';
+      divElement.appendChild(contentElement);
+    }
+  }
 
   function createCard(element, item, text) {
     Object.keys(item).map(eachKey => {
